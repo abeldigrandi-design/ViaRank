e2578412cda0bcf91259036dadbb353763c3a9e9
Abel Digrandi <abeldigrandi@gmail.com>
2026-08-31 17:05:41 -0300

    Configurar puerto para produccion

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const PORT = Number(process.env.PORT) || 3001;
 
 const JWT_SECRET =
   process.env.JWT_SECRET ||
