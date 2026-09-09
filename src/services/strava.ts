5b85b1b8cb6a86cc07233d141a89a98cee9e25ce
Abel Digrandi <abeldigrandi@gmail.com>
2026-09-09 13:20:24 -0300

    Configura callback de Strava segun entorno

--- src/services/strava.ts
+++ src/services/strava.ts
@@ -1,5 +1,5 @@
 const CLIENT_ID = import.meta.env.VITE_STRAVA_CLIENT_ID;
-const REDIRECT_URI = import.meta.env.VITE_STRAVA_REDIRECT_URI;
+const REDIRECT_URI = `${window.location.origin}/exchange_token`;
 
 export function loginWithStrava() {
   console.log("CLIENT_ID:", CLIENT_ID);
@@ -16,3 +16,4 @@ export function loginWithStrava() {
 
   window.location.href = url;
 }
+
